5409d53ff053ca1f0984e5e8f5713819f577ab20
makotonakai <macofandohr1812@gmail.com>
2024-12-30 16:19:20 +0900

    add user controller

---
+++ controllers/user.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/makotonakai/lets-schedule-api-test/models"
+)
+
+var maxIndex = 1
+var mockDB = map[int]*User{}
+
+
+func HealthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Accessible")
+}
+
+func CreateUser(c echo.Context) error {
+
+	newUser := models.User{}
+	err := c.Bind(&newUser)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, config.ErrFailedToBindUser)
+	}
+
+	newUser.Id = maxIndex
+	mockDB[maxIndex] = &newUser
+	maxIndex += 1
+
+	return c.JSON(http.StatusCreated, newUser)
+
+}
