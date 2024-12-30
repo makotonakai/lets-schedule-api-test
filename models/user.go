a75e8cc1cc0b40a8e5c791fe8b4055502217b942
makotonakai <macofandohr1812@gmail.com>
2024-12-30 16:19:12 +0900

    add User model

---
+++ models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+	"regexp"
+	"strings"
+	"gorm.io/gorm"
+)
+
+type User struct {	
+	Id int `json:"id"`	
+	UserName string `json:"user_name"`
+	EmailAddress string `json:"email_address"`
+	Password string `json:"password"`
+	IsAdmin	bool `json:"is_admin"`
+	CanLogin bool `json:"can_login"`	
+	CreatedAt time.Time `json:"created_at"`	
+	UpdatedAt time.Time `json:"updated_at"`	
+}
