a3fc6f16628cbecfaaf102df76a255c068c4ac30
makotonakai <macofandohr1812@gmail.com>
2024-12-31 16:33:46 +0900

    update Initialize method

--- router/router.go
+++ router/router.go
@@ -13,15 +13,15 @@ func Initialize() *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(middleware.BodyDump(bodyDumpHandler))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
 	}))
-	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
 
 	api := e.Group("/api")
 	api.GET("/", controllers.HealthCheck)
 	api.POST("/signup", controllers.CreateUser)
+
+	return e
 
 }
