3ddbca2a9e3e419311d01a74043099ec0b7fa1d5
makotonakai <macofandohr1812@gmail.com>
2024-12-31 18:28:08 +0900

    add Login method

--- controllers/user.go
+++ controllers/user.go
@@ -1,16 +1,30 @@
 package controllers
 
 import (
+	"time"
 	"net/http"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
+
 	"github.com/makotonakai/lets-schedule-api-test/config"
 	"github.com/makotonakai/lets-schedule-api-test/models"
 )
 
 var maxIndex = 1
-var mockDB = map[int]*models.User{}
+var mockDB = map[string]*models.User{}
 
+type JWTCustomClaims struct {
+	Id       int    `json:"uid"`
+	UserName string `json:"name"`
+	jwt.StandardClaims
+}
+
+type Credential struct {
+	Id       int    `json:"id"`
+	UserName string `json:"user_name"`
+	Token string `json:"token"`
+}
 
 func HealthCheck(c echo.Context) error {
 	return c.JSON(http.StatusOK, "Accessible")
@@ -26,9 +40,46 @@ func CreateUser(c echo.Context) error {
 	}
 
 	newUser.Id = maxIndex
-	mockDB[maxIndex] = &newUser
+	mockDB[newUser.UserName] = &newUser
 	maxIndex += 1
 
 	return c.JSON(http.StatusCreated, newUser)
+
+}
+
+func Login(c echo.Context) error {
+
+	u_ := models.User{}
+	err := c.Bind(&u_)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, config.ErrFailedToBindUser)
+	}
+
+	pu := mockDB[u_.UserName]
+	u := *pu
+
+	if u.Id == 0 || u_.UserName != u.UserName || u_.Password != u.Password {
+		return c.JSON(http.StatusUnauthorized, config.ErrLoginFailed)
+	}
+
+	claims := JWTCustomClaims{
+		Id:       u.Id,
+		UserName: u.UserName,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour).Unix(),
+			Issuer:    "test",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signingKey := []byte("secret")
+	t, err := token.SignedString(signingKey)
+
+	return c.JSON(http.StatusOK, Credential{
+		Id:        u.Id,
+		UserName: u.UserName,
+		Token:     t,
+	})
 
 }
